4c8f365bfb30c24e7039c64d2d1287a790ce7fbd
Alex <70957894+Hrapunov@users.noreply.github.com>
2025-03-17 16:56:08 +0300

    Add files via upload

---
+++ Education/src/main/java/org/example/AlwaysTrueTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AlwaysTrueTest {
+
+    @Test
+    public void testAlwaysTrue() {
+        assertTrue(true);
+    }
+}
